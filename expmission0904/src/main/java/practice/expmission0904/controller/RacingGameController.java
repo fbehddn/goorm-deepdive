a843a3fd60d1c8b7d84beadeb1d6d30621c71a41
fbehddn <tom0611@naver.com>
2024-09-04 14:09:05 +0900

    [expmission] 추가미션 - 동적으로 엔진 받기위한 컨트롤러 구현

--- expmission0904/src/main/java/practice/expmission0904/controller/RacingGameController.java
+++ expmission0904/src/main/java/practice/expmission0904/controller/RacingGameController.java
@@ -1,29 +1,35 @@
 package practice.expmission0904.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import practice.expmission0904.domain.Car;
-import practice.expmission0904.domain.ElectricEngine;
-import practice.expmission0904.domain.V6Engine;
+import practice.expmission0904.domain.Engine;
 
 @RestController
 public class RacingGameController {
     private Car car;
+    private Engine v6Engine;
+    private Engine electricEngine;
 
     @Autowired
-    public RacingGameController(Car car) {
+    public RacingGameController(Car car,
+                                @Qualifier("V6") Engine v6Engine,
+                                @Qualifier("Electric") Engine electricEngine) {
         this.car = car;
+        this.v6Engine = v6Engine;
+        this.electricEngine = electricEngine;
     }
 
     @GetMapping("/race")
-    public String startRace(@RequestParam(value = "engine", defaultValue = "v6Engine") String engineType) {
-        if (engineType.equals("v6Engine")) {
-            car = new Car(new V6Engine());
-        } else if (engineType.equals("electricEngine")) {
-            car = new Car(new ElectricEngine());
-        }
+    public String startRace(@RequestParam(value = "engine", defaultValue = "V6") String engineType) {
+        if (engineType.equals("V6")) {
+            car.setEngine(v6Engine);
+        } else if (engineType.equals("Electric")) {
+            car.setEngine(electricEngine);
+        } else return "유효하지 않은 엔진 이름";
         car.startRace();
         return "Race started with " + engineType;
     }
