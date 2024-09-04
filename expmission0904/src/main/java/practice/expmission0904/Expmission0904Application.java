8f47146954473f982d6807742ae55ce43cabf414
fbehddn <tom0611@naver.com>
2024-09-04 13:23:45 +0900

    [expmission] 0904 git init

---
+++ expmission0904/src/main/java/practice/expmission0904/Expmission0904Application.java
@@ -0,0 +1,13 @@
+package practice.expmission0904;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Expmission0904Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Expmission0904Application.class, args);
+    }
+
+}
---
+++ expmission0904/src/test/java/practice/expmission0904/Expmission0904ApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.expmission0904;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Expmission0904ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
