790692a6fefbdfae2b01ddfdca4f4048a571c548
fbehddn <tom0611@naver.com>
2024-09-11 15:30:19 +0900

    [expmission - 0911(book)] API 반환타입 수정 및 book 생성 로직 변경

--- expmission0910/src/main/java/practice/expmission0910/controller/BookController.java
+++ expmission0910/src/main/java/practice/expmission0910/controller/BookController.java
@@ -3,6 +3,7 @@ package practice.expmission0910.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import practice.expmission0910.dto.BookDto;
+import practice.expmission0910.entity.Book;
 import practice.expmission0910.service.BookService;
 
 import java.util.List;
@@ -15,27 +16,32 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public List<BookDto> findAll() {
+    public List<Book> findAll() {
         return bookService.findAll();
     }
 
     // 특정 ID의 책 정보를 조회하는 엔드포인트
     @GetMapping("/search/{id}")
-    public BookDto findById(@PathVariable Long id) {
+    public Book findById(@PathVariable Long id) {
         return bookService.findById(id);
     }
 
-    // 도서 등록
     @PostMapping
-    public void createBook(@RequestBody BookDto bookDto) {
-        bookService.save(bookDto);
+    public Book createBook(@RequestBody BookDto bookDto) {
+        Book book = new Book();
+        book.setTitle(bookDto.getTitle());
+        book.setAuthor(bookDto.getAuthor());
+        book.setPublisher(bookDto.getPublisher());
+        book.setPublished_date(bookDto.getPublished_date());
+
+        return bookService.createBook(book);
     }
+
 
     // 도서 수정
     @PutMapping("/{id}")
-    public void updateBook(@PathVariable Long id, @RequestBody BookDto bookDto) {
-        bookDto.setId(id);
-        bookService.update(bookDto);
+    public Book updateBook(@PathVariable Long id, @RequestBody BookDto bookDto) {
+        return bookService.updateBook(id,bookDto);
     }
 
     // 도서 삭제
