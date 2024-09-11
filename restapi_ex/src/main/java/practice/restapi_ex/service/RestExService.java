4e4d88540936ef146c9f6d5328e8ef7e014f848b
fbehddn <tom0611@naver.com>
2024-09-11 10:26:49 +0900

    0911 2교시 - JPA Hibernate 적용

--- restapi_ex/src/main/java/practice/restapi_ex/service/RestExService.java
+++ restapi_ex/src/main/java/practice/restapi_ex/service/RestExService.java
@@ -1,6 +1,8 @@
 package practice.restapi_ex.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import practice.restapi_ex.dto.ItemDto;
@@ -13,40 +15,46 @@ import java.util.HashMap;
 @Service
 @Slf4j
 public class RestExService {
-    @Autowired
-    private ExMapper exMapper;
+//    @Autowired
+//    private ExMapper exMapper;
+//
+//    @Autowired
+//    private ItemRepository itemRepository;
 
     @Autowired
-    private ItemRepository itemRepository;
+    private SessionFactory sessionFactory;
 
     public boolean registerItem(ItemDto itemDto) {
-        //DB insert
-//        HashMap<String,Object> map = new HashMap<>();
-//        map.put("id",itemDto.getId());
-//        map.put("name",itemDto.getName());
-//
-//        exMapper.registerItem(map);
-        //exMapper.registerItem(itemDto);
+
+        // Hibernate Session 사용하여 데이터 저장
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
 
         ItemEntity itemEntity = new ItemEntity();
         itemEntity.setId(itemDto.getId());
         itemEntity.setName(itemDto.getName());
 
-        itemRepository.save(itemEntity);
+//        itemRepository.save(itemEntity);
+
+        session.save(itemEntity);
+        session.getTransaction().commit();
+        session.close();
 
         log.info("service: register...");
 
-        //DB insert 가 성공했을 경우 true
         return true;
     }
 
     public ItemDto getItem(String id) {
-//        HashMap<String, Object> paramMap = new HashMap<>();
-//        paramMap.put("id", id);
-//
-//        HashMap<String,Object> res = exMapper.findById(paramMap);
 
-        ItemEntity itemEntity = itemRepository.findById(id).get();
+        Session session = sessionFactory.openSession();
+        ItemEntity itemEntity = session.get(ItemEntity.class, id);
+        session.close();
+
+        if(itemEntity == null) {
+            return null;
+        }
+//        ItemEntity itemEntity = itemRepository.findById(id).get();
 
         ItemDto itemDto = new ItemDto();
         itemDto.setId(itemEntity.getId());
