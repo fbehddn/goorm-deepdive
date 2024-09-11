7c715b404d8f0b121bc2ccaff88e7ab830897d37
fbehddn <tom0611@naver.com>
2024-09-11 15:28:17 +0900

    [expmission - 0911(book)] book 엔티티 생성

---
+++ expmission0910/src/main/java/practice/expmission0910/entity/Book.java
@@ -0,0 +1,26 @@
+package practice.expmission0910.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String publisher;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date published_date;
+}
