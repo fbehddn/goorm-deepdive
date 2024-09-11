a401b198f8dd52e0155a3b14ddb34dd1420622ed
fbehddn <tom0611@naver.com>
2024-09-11 15:29:25 +0900

    [expmission - 0911(book)] BookDto 어노테이션 추가

--- expmission0910/src/main/java/practice/expmission0910/dto/BookDto.java
+++ expmission0910/src/main/java/practice/expmission0910/dto/BookDto.java
@@ -2,12 +2,15 @@ package practice.expmission0910.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Data
+@Setter
+@Getter
 public class BookDto {
-    private Long id;
     private String title;
     private String author;
     private String publisher;
