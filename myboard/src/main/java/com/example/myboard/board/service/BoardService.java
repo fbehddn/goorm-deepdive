bae7b99d13c44ccaa3fcf4ece30fcbcbf87d66e5
fbehddn <tom0611@naver.com>
2024-09-27 11:34:42 +0900

    0927 - Board 서비스 생성 로직 구현

---
+++ myboard/src/main/java/com/example/myboard/board/service/BoardService.java
@@ -0,0 +1,27 @@
+package com.example.myboard.board.service;
+
+import com.example.myboard.board.db.BoardEntity;
+import com.example.myboard.board.db.BoardRepository;
+import com.example.myboard.board.model.BoardRequest;
+import com.example.myboard.board.model.BoardResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public BoardResponse create(BoardRequest boardRequest) {
+        var entity = BoardEntity.builder()
+                .boardName(boardRequest.getBoardName())
+                .status("REGISTERED")
+                .build();
+        var saveEntity = boardRepository.save(entity);
+
+        return BoardResponse.builder()
+                .boardId(saveEntity.getId())
+                .status(saveEntity.getStatus())
+                .build();
+    }
+}
