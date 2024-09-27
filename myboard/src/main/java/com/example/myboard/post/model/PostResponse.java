a778c05b933dc0f4d3b06ec286bbc922cc1111bb
fbehddn <tom0611@naver.com>
2024-09-27 11:37:16 +0900

    0927 - Post 엔티티, DTO 및 view DTO 설계

---
+++ myboard/src/main/java/com/example/myboard/post/model/PostResponse.java
@@ -0,0 +1,16 @@
+package com.example.myboard.post.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PostResponse {
+    private Long id;
+    private String status;
+}
