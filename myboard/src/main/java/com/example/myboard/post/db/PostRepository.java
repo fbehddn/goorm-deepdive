181944c5e39d7bc8f84900ce156b9b22e7c3f496
fbehddn <tom0611@naver.com>
2024-09-30 13:40:25 +0900

    0930 - 스프링 JPA 사용하여 삭제되지 않은 게시글만 불러오는 API 구현

--- myboard/src/main/java/com/example/myboard/post/db/PostRepository.java
+++ myboard/src/main/java/com/example/myboard/post/db/PostRepository.java
@@ -2,5 +2,15 @@ package com.example.myboard.post.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<PostEntity,Long> {
+
+    // select * from post where id=? and status=? orderby id desc limit 1
+    // FirstBy 이므로 limit 1 로 제일 첫번째 값만 가져온다
+    public Optional<PostEntity> findFirstByIdAndStatusOrderByIdDesc(Long id, String status);
+
+    // 모든 게시글 조회 시 삭제되지 않은 글만 가져오기
+    public List<PostEntity> findAllByStatusOrderByIdDesc(String status);
 }
