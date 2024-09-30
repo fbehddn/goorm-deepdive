980b2f6c884cc1e68553a46b929a06d30d555c7e
fbehddn <tom0611@naver.com>
2024-09-30 13:42:46 +0900

    0930 - 삭제된 게시글 제외 전체조회 및 삭제 API 구현

--- myboard/src/main/java/com/example/myboard/post/controller/PostApiController.java
+++ myboard/src/main/java/com/example/myboard/post/controller/PostApiController.java
@@ -9,6 +9,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/post")
 @RequiredArgsConstructor
@@ -24,5 +26,15 @@ public class PostApiController {
     @PostMapping("/view")
     public PostViewResponse view(@Valid @RequestBody PostViewRequest postViewRequest) {
         return postService.view(postViewRequest);
+    }
+
+    @GetMapping("/views")
+    public List<PostViewResponse> all() {
+        return postService.all();
+    }
+
+    @PostMapping("/delete")
+    public void delete(@Valid @RequestBody PostViewRequest postViewRequest) {
+        postService.delete(postViewRequest);
     }
 }
