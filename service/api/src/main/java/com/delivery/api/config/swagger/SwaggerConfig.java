8ab8a7c9e0341643785973aaeb46351db62ccb5d
fbehddn <tom0611@naver.com>
2024-11-11 17:09:26 +0900

    1111 - swagger 추가

---
+++ service/api/src/main/java/com/delivery/api/config/swagger/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package com.delivery.api.config.swagger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.core.jackson.ModelResolver;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    public ModelResolver modelResolver(ObjectMapper objectMapper) {
+        return new ModelResolver(objectMapper);
+    }
+}
