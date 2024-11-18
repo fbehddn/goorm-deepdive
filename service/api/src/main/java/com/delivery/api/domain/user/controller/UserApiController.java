6070206cab93cbe9d5a08d84bda174e6ef9c2290
fbehddn <tom0611@naver.com>
2024-11-18 11:01:14 +0900

    [24-11-18] Api 접근성 별로 컨트롤러 분리

--- service/api/src/main/java/com/delivery/api/domain/user/controller/UserApiController.java
+++ service/api/src/main/java/com/delivery/api/domain/user/controller/UserApiController.java
@@ -1,5 +1,6 @@
 package com.delivery.api.domain.user.controller;
 
+import com.delivery.api.domain.user.business.UserBusiness;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/user")
 public class UserApiController {
-
+    private final UserBusiness userBusiness;
 }
--- service/api/src/main/java/com/delivery/api/domain/user/controller/UserOpenApiController.java
+++ service/api/src/main/java/com/delivery/api/domain/user/controller/UserOpenApiController.java
@@ -1,6 +1,13 @@
 package com.delivery.api.domain.user.controller;
 
+import com.delivery.api.common.api.Api;
+import com.delivery.api.domain.user.business.UserBusiness;
+import com.delivery.api.domain.user.controller.model.UserRegisterRequest;
+import com.delivery.api.domain.user.controller.model.UserResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/open-api/user")
 public class UserOpenApiController {
+    private final UserBusiness userBusiness;
 
 }
