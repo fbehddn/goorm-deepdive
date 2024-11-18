e4082ee99b057a1c56d5246cd35b9ae949f4909b
fbehddn <tom0611@naver.com>
2024-11-18 11:03:06 +0900

    [24-11-18] 회원가입 요청을 엔티티로 변환하는 컨버터 메서드 구현

--- service/api/src/main/java/com/delivery/api/domain/user/converter/UserConverter.java
+++ service/api/src/main/java/com/delivery/api/domain/user/converter/UserConverter.java
@@ -1,10 +1,29 @@
 package com.delivery.api.domain.user.converter;
 
 import com.delivery.api.common.annotation.Converter;
+import com.delivery.api.common.error.ErrorCode;
+import com.delivery.api.common.exception.ApiException;
+import com.delivery.api.domain.user.controller.model.UserRegisterRequest;
+import com.delivery.db.user.UserEntity;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Converter
 public class UserConverter {
+    // UserRegisterRequest => UserEntity
+    public UserEntity toEntity(UserRegisterRequest request) {
 
+        return Optional.ofNullable(request)
+                .map(it -> {
+                    //to Entity
+                    return UserEntity.builder()
+                            .name(request.getName())
+                            .email(request.getEmail())
+                            .password(request.getPassword())
+                            .address(request.getAddress())
+                            .build();
+                })
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "UserRegisterRequest is Null"));
+    }
 }
