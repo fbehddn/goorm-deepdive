2c2f0fd2aacf152305292babf1645e4ed871b616
fbehddn <tom0611@naver.com>
2024-11-18 09:49:47 +0900

    11-18 UserEntity 작성 및 UserStatus 설정

---
+++ service/db/src/main/java/com/delivery/db/user/UserEntity.java
@@ -0,0 +1,44 @@
+package com.delivery.db.user;
+
+import com.delivery.db.BaseEntity;
+import com.delivery.db.user.enums.UserStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "user")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class UserEntity extends BaseEntity {
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 100, nullable = false)
+    private String email;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @Column(length = 50, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    @Column(length = 150, nullable = false)
+    private String address;
+
+    private LocalDate registeredAt;
+    private LocalDate unregisteredAt;
+    private LocalDate lastLoginAt;
+}
