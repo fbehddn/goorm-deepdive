b9918eb7eb4caacb9ae269be4f1ad24850c49dfb
fbehddn <tom0611@naver.com>
2024-11-19 10:17:53 +0900

    [24-11-18] 사용자 응답 객체 생성

--- service/api/src/main/java/com/delivery/api/domain/user/controller/model/UserResponse.java
+++ service/api/src/main/java/com/delivery/api/domain/user/controller/model/UserResponse.java
@@ -1,5 +1,10 @@
 package com.delivery.api.domain.user.controller.model;
 
+import com.delivery.db.user.enums.UserStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,4 +15,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class UserResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private UserStatus status;
+    private String address;
+    private LocalDateTime registeredAt;
+    private LocalDateTime unregisteredAt;
+    private LocalDateTime lastLoginAt;
 }
