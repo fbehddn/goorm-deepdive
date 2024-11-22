f0bd4a56f1980fd2fb291a38f1eff950cb2cc110
fbehddn <tom0611@naver.com>
2024-11-22 15:22:28 +0900

    [24-11-22] store 도메인 개발 - 서비스 로직 기본 틀

---
+++ service/api/src/main/java/com/delivery/api/domain/store/service/StoreService.java
@@ -0,0 +1,22 @@
+
+package com.delivery.api.domain.store.service;
+
+import com.delivery.db.store.StoreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class StoreService {
+    private final StoreRepository storeRepository;
+
+    // 필요한 기능 먼저 구현
+    //유효한 스토어가져오기
+
+    // 스토어 등록
+
+    // 카테고리로 스토어 검색
+
+    // 전체 스토어
+
+}
