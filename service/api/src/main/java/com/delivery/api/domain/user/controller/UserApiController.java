68b9031819f6a1e78035f860103985f691304917
fbehddn <tom0611@naver.com>
2024-11-22 10:08:23 +0900

    [24-11-22] resolver 사용하여 공통 인증 처리

--- service/api/src/main/java/com/delivery/api/domain/user/controller/UserApiController.java
+++ service/api/src/main/java/com/delivery/api/domain/user/controller/UserApiController.java
@@ -1,16 +1,14 @@
 package com.delivery.api.domain.user.controller;
 
+import com.delivery.api.common.annotation.UserSession;
 import com.delivery.api.common.api.Api;
 import com.delivery.api.domain.user.business.UserBusiness;
 import com.delivery.api.domain.user.controller.model.UserResponse;
+import com.delivery.api.domain.user.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-
-import java.util.Objects;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,12 +17,8 @@ public class UserApiController {
     private final UserBusiness userBusiness;
 
     @GetMapping("/me")
-    public Api<UserResponse> me() {
-
-        var requestContext = Objects.requireNonNull(RequestContextHolder.getRequestAttributes());
-        var userId = requestContext.getAttribute("userId", RequestAttributes.SCOPE_REQUEST);
-
-        var response = userBusiness.me(Long.parseLong(userId.toString()));
+    public Api<UserResponse> me(@UserSession User user) {
+        var response = userBusiness.me(user.getId());
         return Api.OK(response);
     }
 }
