b57bc4b2c83d4fb9fc6446e85b5fcb779f3c4623
fbehddn <tom0611@naver.com>
2024-11-25 15:43:04 +0900

    [24-11-25] store 서비스 로직 개발 - Service, DTO, Converter, Business 클래스 구현

--- service/api/src/main/java/com/delivery/api/domain/store/model/StoreRegisterRequest.java
+++ service/api/src/main/java/com/delivery/api/domain/store/model/StoreRegisterRequest.java
@@ -1,11 +1,21 @@
 package com.delivery.api.domain.store.model;
 
+import com.delivery.db.store.enums.StoreCategory;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class StoreRegisterRequest {
+    private String name;
+    private String address;
+    private StoreCategory storeCategory;
+    private String thumbnailUrl;
+    private BigDecimal minimumAmount;
+    private BigDecimal minimumDeliveryAmount;
+    private String phoneNumber;
 }
